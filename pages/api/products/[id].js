75abd460c971117ccafbb09acbb7a49b3d74b3b4
Veliko <Kardziev>
2023-06-28 15:43:47 +0200

    Task: Read Products from Database

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -1,13 +1,14 @@
-import { products } from "../../../lib/products";
+import dbConnect from "../../../db/connect.js";
+import Product from "../../../db/models/Product.js";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
   const { id } = request.query;
 
-  const product = products.find((product) => product.id === id);
-
-  if (!product) {
-    return response.status(404).json({ status: "Not Found" });
+  if (request.method === "GET") {
+    const product = await Product.findById(id);
+    return response.status(200).json(product);
+  } else {
+    return response.status(405).json({ message: "Method not allowed" });
   }
-
-  response.status(200).json(product);
 }
--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,5 +1,13 @@
-import { products } from "../../../lib/products";
+import dbConnect from "../../../db/connect.js";
+import Product from "../../../db/models/Product.js";
 
-export default function handler(request, response) {
-  return response.status(200).json(products);
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const products = await Product.find();
+    return response.status(200).json(products);
+  } else {
+    return response.status(405).json({ message: "Method not allowed" });
+  }
 }
