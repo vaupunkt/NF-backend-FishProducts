969a8e85daea88205a005576bfa7c3d4ea3e9118
Veliko <Kardziev>
2023-06-29 12:15:51 +0200

    add create-challenge and solve task

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -4,6 +4,17 @@ import Product from "../../../db/models/Product.js";
 export default async function handler(request, response) {
   await dbConnect();
 
+  if (request.method === "POST") {
+    try {
+      const productData = request.body;
+      await Product.create(productData);
+      response.status(201).json({ status: "Product create." });
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({ error: error.message });
+    }
+  }
+
   if (request.method === "GET") {
     const products = await Product.find();
     return response.status(200).json(products);
